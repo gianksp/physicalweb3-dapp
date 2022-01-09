d68d73d58dfe39eead6667a8fdd6c7ff0e6de05a
Giancarlo Sanchez <gianksp@gmail.com>
2022-01-09 21:21:29 +0000

    Messaging

--- src/App.js
+++ src/App.js
@@ -14,36 +14,40 @@ import Locales from 'ui-component/Locales';
 import NavigationScroll from 'layout/NavigationScroll';
 // import RTLLayout from 'ui-component/RTLLayout';
 import Snackbar from 'ui-component/extended/Snackbar';
-
+import { MoralisProvider } from 'react-moralis';
 // auth provider
 import { FirebaseProvider } from 'contexts/FirebaseContext';
 // import { JWTProvider } from 'contexts/JWTContext';
 // import { Auth0Provider } from 'contexts/Auth0Context';
 
 // ==============================|| APP ||============================== //
+console.log(process.env.REACT_APP_MORALIS_APP_ID);
+console.log(process.env.REACT_APP_MORALIS_SERVER_URL);
 
 const App = () => {
     const customization = useSelector((state) => state.customization);
 
     return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes(customization)}>
-                <CssBaseline />
-                {/* RTL layout */}
-                {/* <RTLLayout> */}
-                <Locales>
-                    <NavigationScroll>
-                        <FirebaseProvider>
-                            <>
-                                <Routes />
-                                <Snackbar />
-                            </>
-                        </FirebaseProvider>
-                    </NavigationScroll>
-                </Locales>
-                {/* </RTLLayout> */}
-            </ThemeProvider>
-        </StyledEngineProvider>
+        <MoralisProvider appId={process.env.REACT_APP_MORALIS_APP_ID} serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}>
+            <StyledEngineProvider injectFirst>
+                <ThemeProvider theme={themes(customization)}>
+                    <CssBaseline />
+                    {/* RTL layout */}
+                    {/* <RTLLayout> */}
+                    <Locales>
+                        <NavigationScroll>
+                            <FirebaseProvider>
+                                <>
+                                    <Routes />
+                                    <Snackbar />
+                                </>
+                            </FirebaseProvider>
+                        </NavigationScroll>
+                    </Locales>
+                    {/* </RTLLayout> */}
+                </ThemeProvider>
+            </StyledEngineProvider>
+        </MoralisProvider>
     );
 };
 
--- src/layout/MainLayout/SmartContractInterface/index.js
+++ src/layout/MainLayout/SmartContractInterface/index.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Typography, Button, Grid, IconButton, TextField, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import { Typography, Button, Grid, IconButton, Box, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 import useConfig from 'utils/configHook';
 import { createTheme } from '@mui/material/styles';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useMoralis } from 'react-moralis';
 
 const customTheme = createTheme({
     components: {
@@ -24,9 +25,11 @@ const customTheme = createTheme({
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SmartContractInterface = () => {
+    const { Moralis, user } = useMoralis();
     const { config = {} } = useConfig();
     const [funcs, setFuncs] = useState([]);
     const [getInputs, setInputs] = useState({});
+    const [getResponses, setResponses] = useState({});
 
     const loadFunctions = (abi = []) => {
         const functions = [];
@@ -71,7 +74,7 @@ const SmartContractInterface = () => {
         navigator.clipboard.writeText(text);
     };
 
-    const getExplorerAddressUrl = () => `${config.blockExplorerUrl}address/${config.contract}`;
+    const getExplorerAddressUrl = (address) => `${config.blockExplorerUrl}address/${address}`;
 
     const displayMetadata = () => (
         <Grid item xs={12}>
@@ -86,7 +89,7 @@ const SmartContractInterface = () => {
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <Button variant="outlined" size="small" href={getExplorerAddressUrl()} target="_blank">
+                        <Button variant="outlined" size="small" href={getExplorerAddressUrl(config.contract)} target="_blank">
                             View in explorer
                         </Button>
                     </Grid>
@@ -94,6 +97,13 @@ const SmartContractInterface = () => {
             </MainCard>
         </Grid>
     );
+
+    const isAddresss = (value = '') => {
+        if (typeof value === 'string' || value instanceof String) {
+            return value.startsWith('0x');
+        }
+        return false;
+    };
 
     const onInputChange = (e) => {
         const components = e.target.id.split('.');
@@ -122,9 +132,139 @@ const SmartContractInterface = () => {
         return inputs;
     };
 
+    const setResponse = (method, value) => {
+        const responses = getResponses;
+        responses[method] = value;
+        setResponses({});
+        setResponses(responses);
+    };
+
+    const invokeSendFunction = async (fn) => {
+        setResponse(fn.name, '');
+        console.log('invoked send');
+        const userInputs = getInputs[fn.name];
+        const fnValues = userInputs ? Object.values(userInputs) : [];
+
+        const web3 = await Moralis.enableWeb3();
+
+        let callStatus = {};
+        try {
+            const encodedFunction = web3.eth.abi.encodeFunctionCall(fn, fnValues);
+
+            const transactionParameters = {
+                to: config.address,
+                from: user.get('ethAddress'),
+                data: encodedFunction
+            };
+
+            const response = await window.ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters]
+            });
+            callStatus = {
+                success: true,
+                message: response
+            };
+        } catch (e) {
+            console.log(e.Error);
+            callStatus = {
+                success: false,
+                message: JSON.stringify(e)
+            };
+        } finally {
+            setResponse(fn.name, callStatus);
+        }
+    };
+
+    const getResponseMessage = (method) => getResponses[method]?.message;
+
+    const getResponseStatus = (method) => getResponses[method]?.success;
+
+    const invokeReadFunction = async (fn) => {
+        setResponse(fn.name, '');
+        console.log('invoked read');
+        const userInputs = getInputs[fn.name];
+        const fnValues = userInputs ? Object.values(userInputs) : [];
+
+        const web3 = await Moralis.enableWeb3();
+        let callStatus = {};
+        try {
+            const NameContract = new web3.eth.Contract(config.abi, config.contract);
+            const response = await NameContract.methods[fn.name](...fnValues).call();
+            callStatus = {
+                success: true,
+                message: response
+            };
+        } catch (e) {
+            callStatus = {
+                success: false,
+                message: JSON.stringify(e)
+            };
+        } finally {
+            setResponse(fn.name, callStatus);
+        }
+    };
+
     const invokeFunction = (fn) => {
         console.log(fn);
-        console.log(getInputs[fn.name]);
+        switch (fn.stateMutability) {
+            case 'view':
+                invokeReadFunction(fn);
+                break;
+            default:
+                invokeSendFunction(fn);
+                break;
+        }
+    };
+
+    const getResponseColor = (name) => {
+        let targetColor;
+        const message = getResponseMessage(name);
+        const status = getResponseStatus(name);
+        switch (status) {
+            case true:
+                targetColor = 'success.main';
+                break;
+            case false:
+                targetColor = 'error.main';
+                break;
+            default:
+                targetColor = 'white';
+                break;
+        }
+        console.log(message);
+        console.log(status);
+        // getResponseMessage(item.name) === null
+        // ? 'white'
+        // : getResponseStatus(item.name)
+        // ? 'success.light'
+        // : 'error.light'
+        return targetColor;
+    };
+
+    const getResponseTextColor = (name) => {
+        let targetColor;
+        const message = getResponseMessage(name);
+        const status = getResponseStatus(name);
+        switch (status) {
+            case true:
+                targetColor = 'white';
+                break;
+            case false:
+                targetColor = 'white';
+                break;
+            default:
+                targetColor = 'black';
+                break;
+        }
+        console.log(message);
+        console.log(status);
+        // getResponseMessage(item.name) === null
+        // ? 'white'
+        // : getResponseStatus(item.name)
+        // ? 'success.light'
+        // : 'error.light'
+        return targetColor;
     };
 
     const displayFunctions = () => {
@@ -132,12 +272,49 @@ const SmartContractInterface = () => {
         funcs.forEach((item) => {
             functions.push(
                 <Grid item xs={12}>
-                    <MainCard>
+                    <MainCard xs={12}>
                         <FormControl variant="standard">
                             {displayInputs(item)}
                             <Button variant="outlined" color={getButtonColor(item.stateMutability)} onClick={() => invokeFunction(item)}>
                                 {item.name}
                             </Button>
+                            <Box
+                                sx={{
+                                    p: 2,
+                                    mt: 1,
+                                    backgroundColor: getResponseColor(item.name),
+                                    color: getResponseTextColor(item.name)
+                                }}
+                            >
+                                {getResponseMessage(item.name) && isAddresss(getResponseMessage(item.name)) ? (
+                                    <Grid container>
+                                        <Grid item xs={12}>
+                                            <Typography variant="body2">
+                                                {trimAddress(getResponseMessage(item.name))}
+                                                <IconButton
+                                                    aria-label="fingerprint"
+                                                    color="primary"
+                                                    onClick={() => copyToClipboard(getResponseMessage(item.name))}
+                                                >
+                                                    <ContentCopyIcon />
+                                                </IconButton>
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Button
+                                                variant="outlined"
+                                                size="small"
+                                                href={getExplorerAddressUrl(getResponseMessage(item.name))}
+                                                target="_blank"
+                                            >
+                                                View in explorer
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                ) : (
+                                    <Typography>{getResponseMessage(item.name)}</Typography>
+                                )}
+                            </Box>
                         </FormControl>
                     </MainCard>
                 </Grid>
